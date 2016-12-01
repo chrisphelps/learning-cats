b68b9639cbedb67ec75c4301a05b34cda41c4d1e
Chris Phelps <cphelps@tendrilinc.com>
2016-12-01 00:11:55 -0700

    Optimize imports and remove commented-out Arbitrary instance

--- src/test/scala/MyOptionSpec.scala
+++ src/test/scala/MyOptionSpec.scala
@@ -1,24 +1,15 @@
-import org.scalatest.{FunSuite, Matchers}
 import MyOption._
-import cats.laws.discipline._
-import org.typelevel.discipline.scalatest.Discipline
 import cats.instances.int._
 import cats.instances.tuple._
 import cats.kernel.Eq
-import org.scalacheck.{Arbitrary, Gen}
+import cats.laws.discipline._
 import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.{FunSuite, Matchers}
+import org.typelevel.discipline.scalatest.Discipline
 
 
 class MyOptionSpec extends FunSuite with Matchers with Discipline {
-
-
-//  implicit val arbMyOption: Arbitrary[MyOption[Int]] = Arbitrary(
-//    Gen.oneOf(
-//      Arbitrary.arbitrary[Int].map(v => MySome(v)),
-//      Gen.const(MyNone)
-//    )
-//  )
-
 
   implicit def arbMyOption[T](implicit a: Arbitrary[T]): Arbitrary[MyOption[T]] =
     Arbitrary(
